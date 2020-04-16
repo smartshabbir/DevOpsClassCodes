a9751219e2ca8975c89a4b9f4587aa721ed0f541
smartshabbir <62828536+smartshabbir@users.noreply.github.com>
2020-04-16 17:31:06 +0530

    removed mobile column

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -76,7 +76,7 @@ public class AddressbookUI extends UI {
 
         contactList
                 .setContainerDataSource(new BeanItemContainer<>(Contact.class));
-        contactList.setColumnOrder("firstName", "lastName", "email", "mobile");
+        contactList.setColumnOrder("firstName", "lastName", "email");
         contactList.removeColumn("id");
         contactList.removeColumn("birthDate");
         contactList.removeColumn("phone");
